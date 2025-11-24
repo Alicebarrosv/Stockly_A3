bada2769793fc0da7c77d91c748abbd83e598fe4
Tulio0macedo <tuliomacedo100@gmail.com>
2025-11-23 21:41:43 -0300

    KAN-24 Teste software funcional

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -5,6 +5,6 @@ public class CalculadoraTest {
     @Test
     public void testsoma() {
         Calc calc = new Calc();
-        assertEquals(6, calc.somar(2, 3));
+        assertEquals(5, calc.somar(2, 3));
     }
 }
